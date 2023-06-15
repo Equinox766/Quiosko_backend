3ff14bc9fd1245e23902659f9167f5d8bdbc6c99
Equinox766 <equinox766@gmail.com>
2023-06-15 11:11:21 -0400

    Creando la tabla de productos con sus funciones

---
+++ database/migrations/2023_06_15_131354_create_productos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('nombre');
+            $table->double('precio');
+            $table->string('imagen');
+            $table->boolean('disponible')->default(1);
+            $table->foreignUuid('categoria_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
