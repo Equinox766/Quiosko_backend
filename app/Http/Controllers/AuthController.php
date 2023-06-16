4243180d7e70dfc11b848cae0f34701343bc0d79
EmanuelLezcano <emanuel.lezcano@biggie.com.py>
2023-06-16 17:43:16 -0400

    Agregando lenguaje español

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegistroRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(RegistroRequest $request)
+    {
+
+    }
+    public function login(Request $request)
+    {
+
+    }
+    public function logout(Request $request)
+    {
+
+    }
+}
---
+++ app/Http/Requests/RegistroRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password as PasswordValidator;
+
+class RegistroRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required','string','min:3','max:255'],
+            'email' => ['required','email','unique:users,email'],
+            'password' => [
+                'required',
+                'confirmed',
+                PasswordValidator::min(8)->letters()->symbols()->numbers(),
+            ],
+        ];
+    }
+}
